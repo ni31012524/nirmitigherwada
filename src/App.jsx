b683d42aa9ef31e84d7df5a028e36d0573fd2a67
Soham Gherwada <222109363+ni31012524@users.noreply.github.com>
2025-07-23 15:12:50 -0400

    res

--- src/App.jsx
+++ src/App.jsx
@@ -65,11 +65,18 @@ export default function App() {
                 <span>LinkedIn</span>
               </a>
               {/* Resume */}
-              <a href="https://github.com/sohamgherwada" target="_blank" rel="noopener noreferrer" className="hover:text-green-300 transition flex items-center space-x-2" title="GitHub">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" className="w-6 h-6">
-                  <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.387.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.416-4.042-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.084-.729.084-.729 1.205.084 1.84 1.236 1.84 1.236 1.07 1.834 2.809 1.304 3.495.997.108-.775.418-1.305.762-1.605-2.665-.305-5.466-1.334-5.466-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.523.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.553 3.297-1.23 3.297-1.23.653 1.653.242 2.873.119 3.176.77.84 1.235 1.91 1.235 3.221 0 4.609-2.803 5.624-5.475 5.921.43.372.823 1.102.823 2.222 0 1.606-.014 2.898-.014 3.293 0 .322.216.694.825.576C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12z" />
+              <a
+                href="https://drive.google.com/file/d/1uTqWAcKUoD0QYnJfOub-RnefbSkN3_3h/view?usp=share_link"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-green-300 transition flex items-center space-x-2"
+                title="Resume"
+              >
+                {/* Resume/Document Icon */}
+                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="w-6 h-6" viewBox="0 0 24 24">
+                  <path d="M14 2H6a2 2 0 00-2 2v16a2 2 0 002 2h12a2 2 0 002-2V8l-6-6zm1 7V3.5L20.5 9H15zM8 13h8v-2H8v2zm0 4h8v-2H8v2z" />
                 </svg>
-                <span>GitHub</span>
+                <span>Resume</span>
               </a>
             </div>
           )}
