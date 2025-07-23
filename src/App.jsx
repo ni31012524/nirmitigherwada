2de6253ea139496ef81b8ec3eba7eb26200dd2c2
Soham Gherwada <222109363+ni31012524@users.noreply.github.com>
2025-07-23 13:40:38 -0400

    link

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ export default function App() {
                 <span>Email</span>
               </a>
               {/* LinkedIn */}
-              <a href="https://linkedin.com/in/nirmitigherwada" target="_blank" rel="noopener noreferrer" className="hover:text-blue-400 transition flex items-center space-x-2" title="LinkedIn">
+              <a href="https://www.linkedin.com/in/nirmiti-gherwada/" target="_blank" rel="noopener noreferrer" className="hover:text-blue-400 transition flex items-center space-x-2" title="LinkedIn">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" className="w-6 h-6">
                   <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-10h3v10zm-1.5-11.268c-.966 0-1.75-.785-1.75-1.75s.784-1.75 1.75-1.75 1.75.784 1.75 1.75-.784 1.75-1.75 1.75zm13.5 11.268h-3v-5.604c0-1.337-.025-3.063-1.868-3.063-1.868 0-2.154 1.459-2.154 2.967v5.7h-3v-10h2.881v1.367h.041c.401-.761 1.379-1.563 2.841-1.563 3.039 0 3.6 2.001 3.6 4.601v5.595zm0 0" />
                 </svg>
