b8216ec8b894c8fffd3887f5311164f8487e80e6
ni31012524 <nirmitigherwada@gmail.com>
2026-02-02 01:43:00 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -66,7 +66,7 @@ export default function App() {
               </a>
               {/* Resume */}
               <a
-                href="https://drive.google.com/file/d/1m5dReF1BN_m-GujILJ2TnbR5HO-MMm6s/view?usp=sharing"
+                href="https://drive.google.com/file/d/17khsnXp03daJXI4z-SNOuSfXchia_zDw/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="hover:text-green-300 transition flex items-center space-x-2"
